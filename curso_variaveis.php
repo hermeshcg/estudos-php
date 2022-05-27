9fbd93436f5a8336deb610bdf971026f6a61a609
Hermes Caretta <38593591+hermeshcg@users.noreply.github.com>
2022-05-27 10:56:40 -0300

    vars

---
+++ curso_variaveis.php
@@ -0,0 +1,47 @@
+<?php
+$nome = 'Hermes';
+$idade = 20;
+$altura = 1.90;
+
+echo "Meu nome é: $nome";
+echo "Minha idade é: $idade";
+echo "Minha altura é: $altura";
+
+
+//
+$nome = 'Jorge da Silva';
+$idade = 21;
+$altura = 1.73;
+
+echo "Meu nome é: $nome";
+echo "Minha idade é: $idade";
+echo "Minha altura é: $altura";
+
+//regras de variaveis
+#tem que colocar $ antes
+#pode qlqr padrão (camelCase, com _)
+#não pode começar com números ou caracteres especiais
+#é case sensitive
+
+//variaveis variaveis?
+$bebida = 'refrigerante'
+
+$$bebida = 'guarana'
+
+#esse echo vai mostrar guarana, pq fez uma troca maluca de variavel ai
+echo $refrigerante; 
+
+$destino = 'cidade';
+$$destino = 'franca';
+
+#vai mostrar franca
+echo $cidade; 
+
+//test 1
+$dev = 'Programação';
+$$dev = 'PHP';
+echo "Ele estuda $dev. <br>";
+echo "Trabalha com uma linguagem chamada $Programação <br>";
+echo "<hr>";
+
+?>
---
+++ tipos_dados.php
@@ -0,0 +1,74 @@
+<?php
+//tipos de dados escalares
+//string :)
+$nome = 'string'
+
+var_dump($nome) #essa func mostra infos sobre a var (tipo, tamanho etc)
+if(is_string($nome)):
+    echo "é string";
+else: 
+    echo "não é string"
+endif;
+
+echo '<hr>';
+
+//int
+$idade = 27;
+var_dump($idade);
+if(is_int($idade)):
+    echo "é int";
+else: 
+    echo "não é int"
+endif;
+
+//float
+$altura = 1.78;
+var_dump($altura);
+if(is_float($altura)):
+    echo "é float";
+else: 
+    echo "não é float"
+endif;
+
+//boolean
+$admin = true;
+var_dump($admin);
+if(is_bool($admin)):
+    echo "é boolean";
+else: 
+    echo "não é boolean"
+endif;
+
+
+
+/**Tipos de dados compostos */
+//array
+$carros = array('Gol', 'Uno', 'Fusca', 10, 2.3, true);
+var_dump($carros);
+if(is_array($carros)):
+    echo "é array";
+else: 
+    echo "não é array"
+endif;
+
+//object
+classe Cliente{
+    public $nome;
+    public function atribuirNome($nome){
+        $this->$nome = $nome
+    }
+}
+
+$cliente = new Cliente()
+$cliente->atribuirNome('Hermes')
+var_dump($cliente)
+if(is_object($cliente)):
+    echo "é object";
+else: 
+    echo "não é object"
+endif;
+
+/* Compostos */
+$cidade = NULL;
+var_dump($cidade)
+?>
