1168889c45154f4cabc834ccccfba415d1ca54c5
Hermes Caretta <hermescaretta@gmail.com>
2022-08-25 11:10:26 -0300

    feeling good

---
+++ 25_login/index.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>Login</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1>Login</h1>
+    <form action="" method="POST">
+      Login: <input type="text" name="login">
+      Senha: <input type="text" name="login">
+      <button type="submit" name="btn-entrar">Entrar</button>
+    </form>
+  </body>
+</html>
